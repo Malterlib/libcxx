4ef2b20f811069ca64ebafea7517600e02a73905
Erik Olofsson <erik@olofsson.info>
2017-01-15 18:40:01 +0100

    Allow disabling new and delete definitions and implementation with _LIBCPP_DISABLE_NEW_DELETE

--- src/new.cpp
+++ src/new.cpp
@@ -59,7 +59,7 @@ __throw_bad_alloc()
 // Implement all new and delete operators as weak definitions
 // in this shared library, so that they can be overridden by programs
 // that define non-weak copies of the functions.
-
+#ifndef _LIBCPP_DISABLE_NEW_DELETE
 _LIBCPP_WEAK
 void *
 operator new(std::size_t size) _THROW_BAD_ALLOC
@@ -299,4 +299,5 @@ operator delete[] (void* ptr, size_t, std::align_val_t alignment) _NOEXCEPT
 }
 
 #endif // !_LIBCPP_HAS_NO_ALIGNED_ALLOCATION
+#endif // _LIBCPP_DISABLE_NEW_DELETE
 #endif // !__GLIBCXX__ && !_LIBCPP_ABI_MICROSOFT && !_LIBCPP_DISABLE_NEW_DELETE_DEFINITIONS
